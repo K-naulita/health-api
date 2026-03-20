b4f6f978b389256219b4830e4f7dfbea652b7b0d
K-naulita <kartikanananaulita@gmail.com>
2026-03-20 22:50:26 +0700

    first commit

---
+++ apl.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import time
+
+app = Flask(__name__)
+
+start_time = time.time()
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "nama": "KARTIKA NANA NAULITA",
+        "nrp": "5025241021",
+        "status": "UP",
+        "timestamp": time.strftime('%Y-%m-%d %H:%M:%S'),
+        "uptime": int(time.time() - start_time)
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
